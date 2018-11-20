ff67dc56b6445e53df9d41c10bc4efb4a31a3429
Ravi Sheth <rrjsheth@gmail.com>
2018-11-20 08:00:23 -0800

    added code for choose answers module. Set up component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'choose-answers',
+    loadChildren: './choose-answers/choose-answers.module#ChooseAnswersModule'
+  },
+  { path: '',   redirectTo: '/choose-answers', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
