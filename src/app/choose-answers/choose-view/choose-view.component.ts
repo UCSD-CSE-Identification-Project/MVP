d52bc82035178c33106ca18fe9d35406e0f7122e
Ravi Sheth <rrjsheth@gmail.com>
2018-11-25 10:07:22 -0800

    added code for rendering finish button, changing images

--- src/app/choose-answers/choose-view/choose-view.component.ts
+++ src/app/choose-answers/choose-view/choose-view.component.ts
@@ -8,17 +8,16 @@ import {forEach} from '@angular/router/src/utils/collection';
   styleUrls: ['./choose-view.component.css']
 })
 export class ChooseViewComponent implements OnInit {
-  // curChoosen;
-  emails = [{ email: 'email1' }, { email: 'email2' }, { email: 'email3' }, { email: 'email4' }];
   boxValues = [{opt: 'a'}, {opt: 'b'}, {opt: 'c'}, {opt: 'd'}, {opt: 'e'}];
   myForm: FormGroup;
   mappedAnswers = {a: [0, false, 'a'], b: [1, false, 'b'], c: [2, false, 'c'], d: [3, false, 'd'], e: [4, false, 'e']};
   imageNames;
   imageIndex;
+  imagesFinished; // if we finish reading all the images
   constructor(private fb: FormBuilder) {
-    // this.curChoosen = [0, 0, 0, 0, 0];
-    this.imageNames = ['imagename1', 'imagename2', 'imagename3'];
+    this.imageNames = this.getImageNames();
     this.imageIndex = 0;
+    this.imagesFinished = false;
   }
 
   ngOnInit() {
@@ -28,17 +27,6 @@ export class ChooseViewComponent implements OnInit {
   }
 
   onChange(option: string, isChecked: boolean) {
-      // alert('in onchange');
-      // console.log(isChecked);
-      // console.log(option);
-      // console.log(this.mappedAnswers[option]);
-    /*
-      if ( isChecked && this.curChoosen[this.mappedAnswers[option]] === 0 ) {
-        this.curChoosen[this.mappedAnswers[option]] = option;
-      } else if (!isChecked && this.curChoosen[this.mappedAnswers[option]] !== 0 ) {
-        this.curChoosen[this.mappedAnswers[option]] = 0;
-      }
-      */
     if ( isChecked && this.mappedAnswers[option][1] === false ) {
       this.mappedAnswers[option][1] = true;
     } else if (!isChecked && this.mappedAnswers[option][1] !== true ) {
@@ -50,28 +38,27 @@ export class ChooseViewComponent implements OnInit {
   checked(option: string){
     return this.mappedAnswers[option][1];
   }
-  submit() {
-    // alert("in here");
+  nextImage() {
+    if (this.imageIndex >= this.imageNames.length) {
+      this.imagesFinished = true;
+      return;
+    }
     const emailFormArray = <FormArray>this.myForm.get('matches') as FormArray;
     const curAnswers = [];
-    /*
-    this.curChoosen.forEach(function (val) {
-      if ( val !== 0 ) {
-        curAnswers.push(val);
-      }
-    });
-    */
+
     Object.values(this.mappedAnswers).forEach(function (value) {
       if ( value[1] === true) {
         curAnswers.push(value[2]);
         value[1] = false;
       }
     });
-    // console.log(curAnswers);
-    // console.log(this.curChoosen);
     emailFormArray.push(new FormControl([this.imageNames[this.imageIndex], curAnswers]));
     this.imageIndex += 1;
-  }
 
+  }
+  getImageNames() {
+    return ['https://www.catster.com/wp-content/uploads/2018/07/Savannah-cat-long-body-shot.jpg',
+            'imagename1', 'imagename2', 'imagename3'];
+  }
 
 }
