575cc1e4d9c0c2cc13d0778d120582d2c9bc7759
Agnes Garcia <agnesgarcia@Agness-MacBook-Pro.local>
2018-11-27 15:51:32 -0800

    Started implementing database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDTtuZlAoBUtxzsgRb-KR0QRS-wlwQL1Hs',
+    authDomain: 'ersp-identification.firebaseapp.com',
+    databaseURL: 'https://ersp-identification.firebaseio.com',
+    projectId: 'ersp-identification',
+    storageBucket: 'ersp-identification.appspot.com',
+    messagingSenderId: '897651924232'
+  }
+
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
