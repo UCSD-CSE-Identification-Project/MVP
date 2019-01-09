28e34f5c984bda06221e1e7f897c0ae08a797654
paulyang <xiy076@ucsd.edu>
2019-01-09 11:10:17 -0800

    user relogin in progress

--- src/app/user-auth/user-auth.module.ts
+++ src/app/user-auth/user-auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { MatDialogModule } from '@angular/material';
 import { UserAuthRoutingModule } from './user-auth-routing.module';
 import { AuthComponent } from './auth/auth.component';
 
@@ -10,6 +10,7 @@ import { AuthComponent } from './auth/auth.component';
   imports: [
     CommonModule,
     FormsModule,
+    MatDialogModule,
     UserAuthRoutingModule
   ]
 })
