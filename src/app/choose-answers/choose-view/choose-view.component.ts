3c3ac3a0fadc451df90087f6dacdb3dc10f8bf32
Ravi Sheth <r1sheth@ucsd.edu>
2019-05-19 16:03:00 -0700

    added code to only choose answers for images that are in previous term and need answering

--- src/app/choose-answers/choose-view/choose-view.component.ts
+++ src/app/choose-answers/choose-view/choose-view.component.ts
@@ -42,7 +42,6 @@ export class ChooseViewComponent implements OnInit {
     // This does not take care of all the cases
     obj.termFinishedAnswering = loadedFromDatabase;
     obj.numImages = obj.imageKeysSorted.length;
-    // console.log(typeof index);
     return obj;
   }
 
@@ -61,12 +60,19 @@ export class ChooseViewComponent implements OnInit {
     };
   }
 
+  /*
+   * Takes in prev or current term string and based on that returns the key images of that term
+   * in the form arr[L00183748Q_19] = "lasldjfhhj2398shkas" where the right hand side is teh key
+   * and the left hand side is teh name of the image
+   *
+   * since iso images have been removed: iso image variable should not change output
+   *
+   */
   getKeyAndIsoImages( prevOrCurrentTerm: string){
     let retVal = {};
     var reversed;
     var tempArr;
     reversed = this.reverseKeyInAllImages(prevOrCurrentTerm);
-    console.log(reversed);
     tempArr = {};
     switch (prevOrCurrentTerm){
       case 'prev':
@@ -74,28 +80,24 @@ export class ChooseViewComponent implements OnInit {
           // retVal = Object.assign(retVal, key);//this.generalInfo.prevTermKeyImages[key]);
           tempArr[reversed[key]] = key;
         }
-        console.log(retVal);
         retVal = Object.assign(retVal, tempArr );
-        console.log(this.generalInfo.prevTermIsoImages);
-        console.log(this.generalInfo.prevTermKeyImages);
         return retVal;
         break;
       case 'curr':
-        retVal = Object.assign({}, this.generalInfo.currTermIsoImages );
         for( let key of Object.keys(this.generalInfo.currTermKeyImages)){
           tempArr[reversed[key]] = key;
         }
-
-        console.log(retVal);
         retVal = Object.assign(retVal, tempArr );
-        console.log(this.generalInfo.currTermIsoImages);
-        console.log(this.generalInfo.currTermKeyImages);
         return retVal;
         break;
       default: break;
     }
   }
 
+  /*
+   * Returns array in the form of arr["keyVal"] = "imageName"
+   *                              arr["kjsh2k3"] = "L123123Q_19"
+   */
   reverseKeyInAllImages( prevOrCurrentTerm: string) {
     let retVal = {};
     switch (prevOrCurrentTerm){
@@ -125,98 +127,74 @@ export class ChooseViewComponent implements OnInit {
     this.totalCurrImages = Object.keys(this.generalInfo.currTermKeyImages).length;
 
     let prevTermIndIsoImages =  this.getKeyAndIsoImages('prev');
-    console.log(prevTermIndIsoImages);
     this.prevTermAnswerObj =
       this.createChooseAnswersTermObj(prevTermIndIsoImages, this.generalInfo.prevTermLoadedFromDatabase, this.generalInfo.prevTermIdVal, 0);
-    console.log(this.prevTermAnswerObj);
 
-    let currTermIndIsoImages = this.getKeyAndIsoImages('curr');
-    console.log(currTermIndIsoImages);
-    this.currTermAnswerObj = this.createChooseAnswersTermObj(currTermIndIsoImages, this.generalInfo.currTermLoadedFromDatabase, this.generalInfo.currTermIdVal, 0);
-
+    this.prevTermAnswerObj.imageIndex = this.startingIndex;
+    this.findNextImageToAnswerInPrevTerm().then(()=>{
+      if ( this.prevTermAnswerObj.imageIndex > this.prevTermAnswerObj.imageKeysSorted.length ){
+        this.prevTermAnswerObj.needGrouping = false;
+        this.prevTermAnswerObj.termFinishedAnswering = true;
+      } else {
+        this.getImageURLsetInHTML(this.prevTermAnswerObj.imageKeysSorted[this.prevTermAnswerObj.imageIndex]);
+      }
+    });
     this.prevTermAnswerObj.needGrouping = !this.generalInfo.prevTermLoadedFromDatabase && !this.generalInfo.prevTermFinished;
-    this.currTermAnswerObj.needGrouping = !this.generalInfo.currTermLoadedFromDatabase && !this.generalInfo.currTermFinished;
-    this.prevTermAnswerObj.termFinishedAnswering = !this.prevTermAnswerObj.needGrouping;
-
-    if (this.prevTermAnswerObj.needGrouping) {
-      this.prevTermAnswerObj.imageIndex = this.startingIndex;
-      this.getImageURLsetInHTML('prev', this.prevTermAnswerObj.imageKeysSorted[this.prevTermAnswerObj.imageIndex]);
-    }
-    else {
-      this.currTermAnswerObj.imageIndex = this.startingIndex;
-      this.getImageURLsetInHTML('curr', this.currTermAnswerObj.imageKeysSorted[this.currTermAnswerObj.imageIndex]);
-    }
-
-    console.log(this.prevTermAnswerObj);
-    console.log(this.currTermAnswerObj);
+    this.prevTermAnswerObj.termFinishedAnswering = false; // !this.prevTermAnswerObj.needGrouping;
   }
 
+  async findNextImageToAnswerInPrevTerm( ){
+    var imgKey;
+    var imgObj;
+    let i = 0;
+    for ( let imgName of this.prevTermAnswerObj.imageKeysSorted.slice(this.prevTermAnswerObj.imageIndex, )){
+      imgKey = this.prevTermAnswerObj.imageNames[imgName];
+      imgObj = await this.db.collection('images').doc(imgKey).ref.get();
+      console.log(imgObj);
+      console.log(imgObj.data());
 
-  setResetTermFinishVariables(prevOrCurrentTerm: string) {
-    if (prevOrCurrentTerm === 'prev') {
+      if ( imgObj.data()["correct_answers"].length === 0 ){
+        this.prevTermAnswerObj.imageIndex += i;
+        return;
+      }
+      i++;
+    }
+    this.prevTermAnswerObj.imageIndex += i;
+  }
+  setResetTermFinishVariables() {
       this.prevTermAnswerObj.termFinishedAnswering = true;
-      this.boxOnScreen = this.createBoxObj();
-      this.getImageURLsetInHTML('curr', this.currTermAnswerObj.imageKeysSorted[0]);
-      //TODO Which of the 2 boolean do we use?
       this.prevTermAnswerObj.needGrouping = false;
       // This one must have
       this.generalInfo.prevTermFinished = true;
-    } else {
-      this.currTermAnswerObj.termFinishedAnswering = true;
       this.imagesFinished = true;
-      this.currTermAnswerObj.needGrouping = false;
-    }
   }
-  getImageURLsetInHTML(prevOrCurr: string, imageKey: string) {
-    console.log(this.prevTermAnswerObj.imageNames[imageKey]);
-    //console.log(this.prevTermAnswerObj.imageNames);
-    //console.log(this.prevTermAnswerObj.imageKeysSorted[0]);
-    //console.log(imageKey);
-    let url = prevOrCurr === "prev" ?
-      this.db.collection('images').doc(this.prevTermAnswerObj.imageNames[imageKey]).ref.get() :
-      this.db.collection('images').doc(this.currTermAnswerObj.imageNames[imageKey]).ref.get();
-    this.boxOnScreen.imageSourceURL = url;
+  getImageURLsetInHTML(imageName: string) {
+    this.boxOnScreen.imageSourceURL = this.db.collection('images').doc(this.prevTermAnswerObj.imageNames[imageName]).ref.get();
   }
-  updateSubGrouping( prevOrCurrentTerm: string, imageKey: string, correctAnswer ){
-    var subGroupingArr;
+
+  updateSubGrouping( imageKeyInDatabase: string, correctAnswer ){
+    console.log(imageKeyInDatabase);
     console.log(this.generalInfo.prevTermKeyImages);
-    if( prevOrCurrentTerm === 'prev'){
-      if ( !(imageKey in this.generalInfo.prevTermKeyImages) ) return;
-      subGroupingArr = this.generalInfo.prevTermKeyImages[imageKey];
-      for ( var key of Object.keys(subGroupingArr) ){
-        if ( subGroupingArr[key] === 'Group' || subGroupingArr[key] === 'Individual') {
-          this.db.collection('images').doc(key).update({correct_answers: correctAnswer});
-        }
-      }
-    }
-
-    else {
-      if ( !(imageKey in this.generalInfo.currTermKeyImages) ) return;
-      subGroupingArr = this.generalInfo.currTermKeyImages[imageKey];
-      for ( var key of Object.keys(subGroupingArr) ){
-        if ( subGroupingArr[key] === 'Group' || subGroupingArr[key] === 'Individual') {
-          this.db.collection('images').doc(key).update({correct_answers: correctAnswer});
-        }
-      }
-
+    var subGroupingArr;
+    if ( !(imageKeyInDatabase in this.generalInfo.prevTermKeyImages) ) return;  // if non key image calls this method
+    subGroupingArr = this.generalInfo.prevTermKeyImages[imageKeyInDatabase];
+    for ( var key of subGroupingArr ){
+      console.log(key);
+      this.db.collection('images').doc(key).update({correct_answers: correctAnswer}).catch(()=> console.error("did not udpate image correctly"));
     }
   }
-  async nextImage(prevOrCurrentTerm: string) {
-    // console.log(this.boxOnScreen.boxAnswer.value);
-    // console.log();
-    let ans = JSON.stringify(this.boxOnScreen.boxAnswer.value);
-    let termAnswerObj = prevOrCurrentTerm === 'prev' ? this.prevTermAnswerObj : this.currTermAnswerObj;
+  async nextImage() {
+    const termAnswerObj = this.prevTermAnswerObj;
     if ((termAnswerObj.numImages - termAnswerObj.imageIndex) <= 1) {
       await this.db.collection('images').doc(termAnswerObj.imageNames[termAnswerObj.imageKeysSorted[termAnswerObj.imageIndex]]).update({
         correct_answers: this.boxOnScreen.boxAnswer.value,
       });
-      this.updateSubGrouping(prevOrCurrentTerm, termAnswerObj.imageNames[termAnswerObj.imageKeysSorted[termAnswerObj.imageIndex]], this.boxOnScreen.boxAnswer.value);
-      this.setResetTermFinishVariables(prevOrCurrentTerm);
+      this.updateSubGrouping(termAnswerObj.imageNames[termAnswerObj.imageKeysSorted[termAnswerObj.imageIndex]], this.boxOnScreen.boxAnswer.value);
+      this.setResetTermFinishVariables();
       return;
-    }
+    } // case for the last image
 
-    console.log("value is " + this.boxOnScreen.boxAnswer.value);
-    console.log("image key is" + termAnswerObj.imageNames[termAnswerObj.imageKeysSorted[termAnswerObj.imageIndex]]);
+
     this.db.collection('images').doc(termAnswerObj.imageNames[termAnswerObj.imageKeysSorted[termAnswerObj.imageIndex]]).update({
       correct_answers: this.boxOnScreen.boxAnswer.value,
     }).then(function () {
@@ -226,19 +204,19 @@ export class ChooseViewComponent implements OnInit {
         console.error("Error updating document: ", error);
       });
 
-    this.updateSubGrouping(prevOrCurrentTerm, termAnswerObj.imageNames[termAnswerObj.imageKeysSorted[termAnswerObj.imageIndex]], this.boxOnScreen.boxAnswer.value);
+    this.updateSubGrouping(termAnswerObj.imageNames[termAnswerObj.imageKeysSorted[termAnswerObj.imageIndex]], this.boxOnScreen.boxAnswer.value);
 
     this.boxOnScreen = this.createBoxObj();
-    termAnswerObj.imageIndex += 1;
-    // Need to update indices
-    console.log(termAnswerObj.imageKeysSorted);
-    console.log(termAnswerObj.imageIndex);
-    console.log(termAnswerObj.imageNames);
-    this.getImageURLsetInHTML(prevOrCurrentTerm, termAnswerObj.imageKeysSorted[termAnswerObj.imageIndex]);
+    this.findNextImageToAnswerInPrevTerm().then(()=>{
+      if ( termAnswerObj.imageIndex >= termAnswerObj.imageKeysSorted.length) {
+        this.setResetTermFinishVariables();
+        return;
+      }
+      this.getImageURLsetInHTML(termAnswerObj.imageKeysSorted[termAnswerObj.imageIndex]);
+    });
   }
 
   boxChecked(isChecked: boolean) {
-    console.log(isChecked);
     if (isChecked) {
       this.boxOnScreen.numBoxesChecked++;
     } else {
@@ -286,9 +264,6 @@ export class ChooseViewComponent implements OnInit {
       currTermInfo: this.generalInfo.currTerm,
       lectureOrImageIndex: this.prevTermAnswerObj.needGrouping ? this.prevTermAnswerObj.imageIndex : this.currTermAnswerObj.imageIndex
     };
-    console.log(this.prevTermAnswerObj.needGrouping);
-    console.log(this.prevTermAnswerObj.imageIndex)
-    console.log(this.currTermAnswerObj.imageIndex);
     this.authService.setStorage("session", object, "termData");
     return false;
   }
